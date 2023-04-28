db8ab75891cea4c547e44ab9c40f7b8b82b6ac37
Dzianis Panasiuk <dzianispanasiuk@gmail.com>
2023-04-29 01:00:56 +0300

    updated class FloatTypeCasting

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -3,5 +3,6 @@ package lang.print.gaps.task6;
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
         int number = (int) Math.floor(numberToBeRounded);
+        System.out.println(number);
     }
 }
